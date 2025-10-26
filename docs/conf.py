5029c3064f201908c57dfe6d72cf1ae4a1ebd173
Ashwin V. Mohanan <ashwin.mohanan@enccs.se>
2025-10-26 23:57:05 +0100

    Prepare docs with myst-example and bibtex support

--- docs/conf.py
+++ docs/conf.py
@@ -1,3 +1,4 @@
+from sphinx.application import Sphinx
 # Configuration file for the Sphinx documentation builder.
 #
 # For the full list of built-in configuration values, see the documentation:
@@ -14,9 +15,10 @@ release = "0.1.0b0"
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ["sphinx_lesson", "sphinx_evita"]
+extensions = ["sphinx_lesson", "sphinx_evita", "sphinx_design", "sphinxcontrib.bibtex"]
 
 myst_enable_extensions = ["colon_fence", "attrs_inline"]
+bibtex_bibfiles = ["references.bib"]
 
 templates_path = ["_templates"]
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
@@ -28,3 +30,22 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 html_theme = "alabaster"  # This should be overriden by sphinx_evita config-init hook
 html_title = project
 html_static_path = ["_static"]
+
+html_css_files = ["overrides.css"]
+
+# def setup(app):
+#     from myst_parser._docs import MystExampleDirective
+#     app.add_directive("myst-example", MystExampleDirective)
+
+
+def setup(app: Sphinx):
+    """Add functions to the Sphinx setup."""
+    from myst_parser._docs import (
+        MystAdmonitionDirective,
+        MystExampleDirective,
+        MystLexer,
+    )
+
+    app.add_directive("myst-example", MystExampleDirective)
+    app.add_directive("myst-admonitions", MystAdmonitionDirective)
+    app.add_lexer("myst", MystLexer)
